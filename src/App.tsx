1f5de26cd410621074ff2c9f86289859deac1c76
Rubén Valls <r.vallsaparici@gmail.com>
2023-05-29 10:17:12 +0200

    added router and folder structure related to pages home/signup/users

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import './App.css'
+import { Routes, Route } from "react-router-dom";
+import Home from './pages/home/Home';
+import Singup from './pages/singup/Singup';
+import Users from './pages/users/Users';
 
 function App() {
 
   return (
     <>
-      App
+      HEADER
+      <Routes>
+        <Route index path="/" element={<Home />}></Route>
+        <Route path="/signup" element={<Singup />}></Route>
+        <Route path="/users" element={<Users />}></Route>
+      </Routes>
     </>
   )
 }
