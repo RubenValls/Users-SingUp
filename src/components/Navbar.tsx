515cb8a0ab91d75bd8d8a53da56c0195ec513223
Rubén Valls <r.vallsaparici@gmail.com>
2023-05-29 11:14:48 +0200

    Added changes to navbar and box structure.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,10 +8,18 @@ import {
     useDisclosure,
     Button,
     VStack,
-  } from '@chakra-ui/react'
+    Flex,
+    Text,
+    Center
+} from '@chakra-ui/react'
 import { NavLink } from "react-router-dom";
 import { useRef } from 'react'
 import {RiMenuUnfoldFill} from 'react-icons/ri'
+import {FiUsers} from 'react-icons/fi'
+import {
+    AiOutlineHome,
+    AiOutlineUserAdd
+} from 'react-icons/ai'
 
 export default function Navbar() {
     const { isOpen, onOpen, onClose } = useDisclosure()
@@ -35,24 +43,37 @@ export default function Navbar() {
                 <DrawerBody>
                     <VStack
                         spacing={4}
+                        align='left'
                     >
                         <NavLink
                             to={"/"}
                             onClick={onClose}
                         >
-                            Home
+                            <Flex>
+                                <Center>
+                                    <AiOutlineHome/> <Text ml='5px'>Home</Text>
+                                </Center>
+                            </Flex>
                         </NavLink>
                         <NavLink
                             to={"/signup"}
                             onClick={onClose}
                         >
-                            User - Sign Up
+                            <Flex>
+                                <Center>
+                                    <AiOutlineUserAdd/> <Text ml='5px'>User - Sign Up</Text>
+                                </Center>
+                            </Flex>
                         </NavLink>
                         <NavLink
                             to={"/users"}
                             onClick={onClose}
                         >
-                            Users Panel
+                            <Flex>
+                                <Center>
+                                    <FiUsers/> <Text ml='5px'>Users Panel</Text>
+                                </Center>
+                            </Flex>
                         </NavLink>
                     </VStack>
                 </DrawerBody>
--- src/pages/singup/Singup.tsx
+++ src/pages/singup/Singup.tsx
@@ -1,6 +1,11 @@
+import { Box } from "@chakra-ui/react";
 
 export default function Singup() {
   return (
-    <div>Singup</div>
+    <>
+        <Box h='auto' w='100%' ml='10px' mr='10px'>
+          Sign Up
+        </Box>
+    </>
   )
 }
