3ef2ce5de35057a646a0061b5a04290d5d5d938c
Rubén Valls <r.vallsaparici@gmail.com>
2023-05-30 11:51:13 +0200

    added selection at datatable

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -2,16 +2,21 @@ import { Box } from "@chakra-ui/react";
 import { DataTable } from 'primereact/datatable';
 import { Column } from "primereact/column";
 import { getInitialValues } from "../../utils/localStorage";
+import { useState } from "react";
 
 export default function Users() {
-
+  const [selectedUser, setSelectedUser] : any = useState(null);
   const usersData : any = getInitialValues()
   
+  const onRowSelect = (event : any) => {
+    console.log(event?.data)
+};
+
   return (
     <>
         <Box h='auto' w='100%' ml='10px' mr='10px'>
           <Box mt='50px'>
-            <DataTable value={JSON.parse(usersData)} tableStyle={{ minWidth: 'auto' }}>
+            <DataTable value={JSON.parse(usersData)} selectionMode="single" selection={selectedUser} onSelectionChange={(e) => setSelectedUser(e?.value)} onRowSelect={onRowSelect} tableStyle={{ minWidth: 'auto' }}>
               <Column field='name' header='Name' sortable></Column>
               <Column field='surname' header='Surname' sortable></Column>
               <Column field='email' header='Email' sortable></Column>
@@ -19,7 +24,6 @@ export default function Users() {
               <Column field='city' header='City'></Column>
               <Column field='address' header='Address'></Column>
               <Column field='password' header='Password'></Column>
-              <Column rowEditor headerStyle={{ width: '10%', minWidth: '8rem' }} bodyStyle={{ textAlign: 'center' }}></Column>
             </DataTable>
           </Box>
         </Box>
