5889b5f6af197fcbd058ec7c96e31ce2670ac25f
Rubén Valls <r.vallsaparici@gmail.com>
2023-06-01 12:42:26 +0200

    added information to signup page

--- src/pages/singup/Singup.tsx
+++ src/pages/singup/Singup.tsx
@@ -1,19 +1,28 @@
 import { 
   Box,
   Center,
+  Divider,
+  VStack,
 } from "@chakra-ui/react";
 import FormikSignUpComponent from "../../components/formikSignUpComponent";
+import AccordionSignUpComponent from "../../components/AccordionSignUpComponent";
 
 export default function Singup() {
 
 
   return (
     <>
+      <VStack>
         <Center m='auto'>
-        <Box h='auto' w='100%' m='10px' mr='10px' mt='47px'>
+        <Box h='auto' w='100%' m='10px' mr='10px' mt='47px' mb='25px'>
           <FormikSignUpComponent />
         </Box>
         </Center>
+        <Divider w='100vw' />
+        <Box h='auto' w='100%'>
+          <AccordionSignUpComponent />
+        </Box>
+      </VStack>
     </>
   )
 }
